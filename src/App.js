0ba8781109303fe7c82f5358a42b65dd95763c6e
Parth sarthi <sarthiparth683@gmail.com>
2023-11-02 13:36:02 -0700

    comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,333 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  function functionName() {
+    const person = {
+      name: "John",
+      age: 30,
+      city: "New York",
+    };
+    console.log(person.name); // John
+    console.log(person["age"]); // 30
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      {functionName()}
+      <span style={{ color: "blue" }}>
+        {" "}
+        <b>1. Functions in JavaScript</b> <br />
+        <img style={{ width: 1000 }} src=" .\images\1.function\1.png " alt="" />
+        <img
+          style={{ width: 1000 }}
+          src=" .\images\1.function\2.png "
+          alt=""
+        />{" "}
+        <br />
+        <img style={{ width: 711 }} src=".\images\1.function\3.png" alt="" />
+      </span>{" "}
+      <br />
+      2. if else, in JavaScript <br />
+      <img src=".\images\2.if else\Screenshot (19).png" alt="" />
+      <img src=".\images\2.if else\Screenshot (21).png" alt="" />
+      <br />
+      3. switch statement in JavaScript <br />
+      <img src=".\images\3.switch\Screenshot (22).png" alt="" />
+      <img src=".\images\3.switch\Screenshot (23).png" alt="" />
+      <br />
+      4. loops:-
+      <br />
+      <ol type="i">
+        <li>
+          for loop: The most common type of loop, which executes a block of code
+          a fixed number of times.
+        </li>
+        <img src=".\images\4.loops\1.for loop\Screenshot (24).png" alt="" />
+        <img src=".\images\4.loops\1.for loop\Screenshot (25).png" alt="" />
+
+        <li>
+          while loop: Executes a block of code as long as a specified condition
+          is true.
+        </li>
+        <img src=".\images\4.loops\2.while loop\Screenshot (26).png" alt="" />
+        <img src=".\images\4.loops\2.while loop\Screenshot (27).png" alt="" />
+        <img src=".\images\4.loops\2.while loop\Screenshot (28).png" alt="" />
+        <li>
+          do...while loop: Executes a block of code at least once, and then
+          continues to execute it as long as a specified condition is true.
+        </li>
+        <img
+          src=".\images\4.loops\3.do while loop\Screenshot (29).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\3.do while loop\Screenshot (30).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\3.do while loop\Screenshot (31).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\3.do while loop\Screenshot (32).png"
+          alt=""
+        />
+
+        <li>for...in loop: Iterates over the properties of an object.</li>
+        <img src=".\images\4.loops\4.for in loop\Screenshot (33).png" alt="" />
+        <img src=".\images\4.loops\4.for in loop\Screenshot (34).png" alt="" />
+        <img src=".\images\4.loops\4.for in loop\Screenshot (35).png" alt="" />
+        <li>for...of loop: Iterates over the values of an iterable object.</li>
+        <img src=".\images\4.loops\5.for of loop\Screenshot (36).png" alt="" />
+        <img src=".\images\4.loops\5.for of loop\Screenshot (37).png" alt="" />
+        <img src=".\images\4.loops\5.for of loop\Screenshot (38).png" alt="" />
+        <img src=".\images\4.loops\5.for of loop\Screenshot (39).png" alt="" />
+        <img src=".\images\4.loops\5.for of loop\Screenshot (40).png" alt="" />
+
+        <li>
+          labeled loop: Allows you to break out of a loop from within another
+          loop.
+        </li>
+        <img src=".\images\4.loops\6.labeled loop\Screenshot (41).png" alt="" />
+        <img src=".\images\4.loops\6.labeled loop\Screenshot (42).png" alt="" />
+        <img src=".\images\4.loops\6.labeled loop\Screenshot (43).png" alt="" />
+        <li>
+          continue statement: Skips the rest of the current iteration of a loop
+          and goes to the next iteration.
+        </li>
+        <img
+          src=".\images\4.loops\7.continue statement\Screenshot (44).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\7.continue statement\Screenshot (45).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\7.continue statement\Screenshot (46).png"
+          alt=""
+        />
+        <li>
+          break statement: Terminates the execution of a loop in for loop , for
+          in loop , while loop , do while loop etc...
+        </li>
+        <img
+          src=".\images\4.loops\8.break statement\Screenshot (47).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\8.break statement\Screenshot (48).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\8.break statement\Screenshot (49).png"
+          alt=""
+        />
+        <img
+          src=".\images\4.loops\8.break statement\Screenshot (50).png"
+          alt=""
+        />
+      </ol>
+      <br />
+      5. Error handeling, try catch blocks. <br />
+      <img src=".\images\5.try catch\1.png" alt="" />
+      <img src=".\images\5.try catch\2.png" alt="" />
+      <img src=".\images\5.try catch\3.png" alt="" />
+      <img src=".\images\5.try catch\4.png" alt="" /> <br />
+      6. Variables:-Var, let, const. <br />
+      <img src=".\images\6.variables\Screenshot (55).png" alt="" /> <br />
+      <p>
+        <br />
+        Scope refers to the part of the program where a variable can be
+        accessed. Function scope means that a variable declared with var inside
+        a function can be accessed from anywhere inside that function, as well
+        as from any other function that is nested within it. Global scope means
+        that a variable declared with var outside of any function can be
+        accessed from anywhere in the program.
+        <br />
+        <br />
+        Block scope means that a variable declared with let or const inside a
+        block of code (such as a function body, if statement, or for loop) can
+        only be accessed from inside that block. This helps to prevent
+        accidental reassignment of variables and makes code more readable and
+        maintainable.
+        <br />
+        <br />
+        Reassignment refers to the ability to change the value of a variable
+        after it has been declared. Variables declared with var and let can be
+        reassigned, but variables declared with const cannot.
+        <br />
+        <br />
+        Hoisting is a mechanism in JavaScript that moves all variable
+        declarations to the top of their scope. This means that variables
+        declared with var and let can be accessed before they are declared, but
+        variables declared with const cannot.
+        <br />
+        <br />
+        Initialization refers to the process of assigning a value to a variable
+        when it is declared. Variables declared with var and let can be
+        initialized or not, but variables declared with const must be
+        initialized.
+        <br />
+        <br />
+      </p>
+      7. Data Types:- <br />
+      JavaScript has seven primitive data types: <br />
+      Number: A numeric value.
+      <br />
+      String: A sequence of characters.
+      <br />
+      Boolean: A logical value, either true or false.
+      <br />
+      Null: A special value that represents the absence of a value.
+      <br />
+      Undefined: A special value that represents a value that has not yet been
+      assigned.
+      <br />
+      Bigint: An integer with arbitrary precision.
+      <br />
+      Symbol: A unique value that can be used to identify objects.
+      <br />
+      <br />
+      Non-primitive data types
+      <br />
+      JavaScript also has a number of non-primitive data types, which are
+      objects. The most common non-primitive data types are:
+      <br />
+      Array: An ordered collection of values.
+      <br />
+      Object: A collection of key-value pairs.
+      <br />
+      Function: A block of code that can be executed.
+      <br />
+      Date: A representation of a date and time.
+      <br />
+      <img src=".\images\7.Data types\Screenshot (56).png" alt="" />
+      <img src=".\images\7.Data types\Screenshot (57).png" alt="" />
+      <img src=".\images\7.Data types\Screenshot (58).png" alt="" />
+      <br />
+      8. Operators in JavaScript:-
+      <br />
+      <img src=".\images\8.operators\Screenshot (59).png" alt="" /> <br />
+      There are 7 types of operators in JavaScript:
+      <br />
+      Arithmetic Operators: Used to perform arithmetic operations such as
+      addition, subtraction, multiplication, and division.
+      <br />
+      Assignment Operators: Used to assign values to variables.
+      <br />
+      Comparison Operators: Used to compare two values and return a Boolean
+      value (true or false).
+      <br />
+      Logical Operators: Used to combine two or more Boolean values to produce a
+      single Boolean value.
+      <br />
+      Ternary Operator: Used to provide a concise way to write conditional
+      statements.
+      <br />
+      Type Operator: Used to check the data type of a value.
+      <br />
+      Bitwise Operators: Used to perform bit-level operations on values.
+      <br />
+      <br />
+      9. Arrays and their methods...
+      <br />
+      <img src=".\images\9.Arrays\Screenshot (60).png" alt="" />
+      concat(): Merges two or more arrays into a new array. <br />
+      constructor(): Creates a new array object. <br />
+      copyWithin(): Copies elements within the array. <br />
+      entries(): Returns an iterator over the key-value pairs of the array.{" "}
+      <br />
+      every(): Checks if every element in the array passes a test. <br />
+      fill(): Fills the elements of the array with a static value. <br />
+      filter(): Creates a new array with all of the elements in the input array
+      that pass a test. <br />
+      find(): Returns the value of the first element in the array that passes a
+      test. <br />
+      findIndex(): Returns the index of the first element in the array that
+      passes a test. <br />
+      flat(): Flattens a multidimensional array into a single array. <br />
+      flatMap(): Flattens a multidimensional array into a single array and
+      applies a callback function to each element. <br />
+      forEach(): Executes a callback function on each element of the array.{" "}
+      <br />
+      from(): Creates a new array from an iterable object. <br />
+      includes(): Checks if an element is present in the array. <br />
+      indexOf(): Returns the index of the first occurrence of an element in the
+      array. <br />
+      isArray(): Checks if the value passed to the function is an array. <br />
+      join(): Joins the elements of the array into a string. <br />
+      keys(): Returns an iterator over the keys of the array. <br />
+      lastIndexOf(): Returns the index of the last occurrence of an element in
+      the array. <br />
+      length: Returns the number of elements in the array. <br />
+      map(): Creates a new array with the results of applying a callback
+      function to each element of the input array. <br />
+      pop(): Removes and returns the last element of the array. <br />
+      prototype: The prototype property of the Array object contains a number of
+      methods that can be used to manipulate arrays. <br />
+      push(): Adds one or more elements to the end of the array. <br />
+      reduce(): Reduces the elements of the array to a single value by applying
+      a callback function to each element. <br />
+      reduceRight(): Reduces the elements of the array to a single value by
+      applying a callback function to each element, in reverse order. <br />
+      reverse(): Reverses the order of the elements in the array. <br />
+      shift(): Removes and returns the first element of the array. <br />
+      slice(): Creates a new array from a subset of the elements in the input
+      array. <br />
+      some(): Checks if at least one element in the array passes a test. <br />
+      sort(): Sorts the elements of the array in ascending or descending order.{" "}
+      <br />
+      splice(): Adds, removes, and/or replaces elements in the array. <br />
+      toString(): Returns a string representation of the array. <br />
+      unshift(): Adds one or more elements to the beginning of the array. <br />
+      valueOf(): Returns the primitive value of the array. <br /> <br />
+      10. Objects
+      <br />
+      <img src=".\images\10. Object\Screenshot (61).png" alt="" /> <br />
+      11. Events:-onclick,mouse movements etc... <br />
+      There are many different types of events in JavaScript, some of the most
+      common include: <br /> <br />
+      Mouse events: These events are fired when the user interacts with the
+      mouse, such as clicking, moving, or hovering over an element. <br />
+      Keyboard events: These events are fired when the user presses or releases
+      a key on the keyboard. <br />
+      Form events: These events are fired when the user interacts with a form
+      element, such as submitting a form or entering data into a field. <br />
+      Window events: These events are fired when something happens to the
+      browser window, such as resizing or closing. <br />
+      Document events: These events are fired when something happens to the HTML
+      document, such as loading or unloading. <br /> <br />
+      JavaScript can respond to events in two ways: <br />
+      1.)Inline event handlers: These are functions that are assigned to the on
+      attribute of an HTML element. For example, the following code assigns a
+      function to the onclick event handler of a button:
+      <br />
+      <img src=".\images\11.Events\1.png" alt="" />
+      <img src=".\images\11.Events\2.png" alt="" /> <br />
+      2.)AddEventListener() method: <br />
+      <img src=".\images\11.Events\3.png" alt="" />
+      <img src=".\images\11.Events\4.png" alt="" />
+      <img src=".\images\11.Events\5.png" alt="" />
+      <br />
+      12. DOM <br />
+      The Document Object Model (DOM) is a programming interface for HTML
+      documents. It represents the page so that programs can change the document
+      structure, style, and content. The DOM represents the document as nodes
+      and objects; that way, programming languages can interact with the page.
+      <br />
+      <br />
+      JavaScript is the most common programming language used to interact with
+      the DOM. JavaScript can be used to:
+      <br />
+      <br />
+      <div style={{margin:20}} >
+      <li>Add, remove, or modify HTML elements</li>
+      <li>Change the style of HTML elements</li>
+      <li>Get or set the content of HTML elements</li>
+      <li>Respond to user events, such as clicks and mouse movements</li>
+      </div>
+      <br />
+      <img src=".\images\12.DOM\Screenshot (68).png" alt="" />
     </div>
   );
 }
